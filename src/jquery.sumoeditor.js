cdd054ce7d6c0ffab9b9fc2da551277cd9a011e1
Hemant Negi <hnegi@google.com>
2017-11-13 12:42:56 +0530

    refractoring of setPos

--- src/jquery.sumoeditor.js
+++ src/jquery.sumoeditor.js
@@ -1254,47 +1254,47 @@
         * @param {number} pos The position offset.
         * */
         setPos: function ($E, pos) {
-            // TODO: Needs refactoring this can be implemented by selection apis
-            var createRange = function (node, chars, range) {
-                if (!range) {
-                    range = document.createRange()
-                    range.selectNode(node);
-                    range.setStart(node, 0);
-                }
 
-                if (chars.count === 0) {
-                    range.setEnd(node, chars.count);
-                } else if (node && chars.count > 0) {
-                    if (node.nodeType === Node.TEXT_NODE) {
-                        if (node.textContent.length < chars.count) {
-                            chars.count -= node.textContent.length;
+            var o = this,
+                createRange = function (node, chars, range) {
+                    if (!range) {
+                        range = document.createRange()
+                        range.selectNode(node);
+                        range.setStart(node, 0);
+                    }
+
+                    if (chars.count === 0) {
+                        range.setEnd(node, chars.count);
+                    } else if (node && chars.count > 0) {
+                        if (node.nodeType === Node.TEXT_NODE) {
+                            if (node.textContent.length < chars.count) {
+                                chars.count -= node.textContent.length;
+                            } else {
+                                range.setEnd(node, chars.count);
+                                chars.count = 0;
+                            }
                         } else {
-                            range.setEnd(node, chars.count);
-                            chars.count = 0;
-                        }
-                    } else {
-                        for (var lp = 0; lp < node.childNodes.length; lp++) {
-                            range = createRange(node.childNodes[lp], chars, range);
+                            for (var lp = 0; lp < node.childNodes.length; lp++) {
+                                range = createRange(node.childNodes[lp], chars, range);
 
-                            if (chars.count === 0) {
-                                break;
+                                if (chars.count === 0) {
+                                    break;
+                                }
                             }
                         }
                     }
-                }
 
-                return range;
+                    return range;
             };
 
             if (pos >= 0) {
-                var selection = window.getSelection();
-
-                var range = createRange($E[0], {count: pos});
+                var sel = o.O.selection.obj(),
+                    range = createRange($E[0], {count: pos});
 
                 if (range) {
                     range.collapse(false);
-                    selection.removeAllRanges();
-                    selection.addRange(range);
+                    sel.removeAllRanges();
+                    sel.addRange(range);
                 }
             }
 
